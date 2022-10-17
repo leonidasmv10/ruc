84f769598e348221fade1d28568df98262c2533f
Yordy Leonidas <yordy.lmv.2000@gmail.com>
2022-10-17 14:55:17 +0200

    add thread test

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,44 @@
 ﻿#include "../config.h"
 
+#include <string>
+#include <iostream>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+#include <spdlog\spdlog.h>
 #include "libzippp.h"
 
-#include <iostream>
 #include <fstream>
-#include <string>
-
-
 #include <chrono>
 #include <ctime> 
 #include <thread>
-
-//#include <mysql/jdbc.h>
-
 #include <vector>
 #include <unordered_map>
 
-
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <ctime> 
+#include <thread>
+#include <mysql/jdbc.h>
+#include <vector>
+#include <unordered_map>
+#include <stdio.h>
+#include <curl/curl.h>
+#include <future>
 
 using namespace libzippp;
 using namespace std;
 
-#include <iostream>
-#include <fstream>
-#include <string>
+GLFWwindow* window;
+bool is_zip = false;
 
-#include <chrono>
-#include <ctime> 
-#include <thread>
-
-#include <mysql/jdbc.h>
-
-#include <vector>
-#include <unordered_map>
-
-#include <stdio.h>
-#include <curl/curl.h>
-/* For older cURL versions you will also need
-#include <curl/types.h>
-#include <curl/easy.h>
-*/
-#include <string>
+void DockSpace();
 
 size_t write_data(void* ptr, size_t size, size_t nmemb, FILE* stream) {
 	size_t written = fwrite(ptr, size, nmemb, stream);
@@ -48,246 +46,249 @@ size_t write_data(void* ptr, size_t size, size_t nmemb, FILE* stream) {
 }
 
 
+void download_zip();
 
-std::vector<std::string> split_iterator(const std::string& str)
+void loop();
+
+std::thread hilo_1(loop);
+//std::thread hilo_2(download_zip);
+
+std::thread::id main_thread_id = std::this_thread::get_id();
+
+int main()
 {
-	std::vector<std::string> resultado;
+	if (main_thread_id == std::this_thread::get_id())
+		std::cout << "This is the main thread.\n";
+	else
+		std::cout << "This is not the main thread.\n";
 
-	std::string::const_iterator itBegin = str.begin();
-	std::string::const_iterator itEnd = str.end();
-
-	int numItems = 1;
-	for (std::string::const_iterator it = itBegin; it != itEnd; ++it)
-		numItems += *it == '|';
-
-	resultado.reserve(numItems);
-
-	for (std::string::const_iterator it = itBegin; it != itEnd; ++it)
+	std::future<void> fut{};
+	if (true)  // 0 just for testing; your condition would come here
 	{
-		if (*it == '|')
-		{
-			resultado.push_back(std::string(itBegin, it));
-			itBegin = it + 1;
-		}
+		fut = std::async(std::launch::async, download_zip);
 	}
 
-	if (itBegin != itEnd)
-		resultado.push_back(std::string(itBegin, itEnd));
-
-	return resultado;
-}
-
-std::string find_ruc(const std::string& str)
-{
-
-	std::string::const_iterator itBegin = str.begin();
-	std::string::const_iterator itEnd = str.end();
-
-	for (std::string::const_iterator it = itBegin; it != itEnd; ++it)
+	if (fut.valid())
 	{
-		if (*it == '|')
-		{
-			return std::string(itBegin, it);
+		fut.get();
+		spdlog::error("Termina sync");
 
-		}
-	}
-	return "";
-}
-
-std::string formatInsert(std::string& texto)
-{
-	std::string text = "";
-
-	for (int i = 0; i < texto.size(); i++)
-	{
-		if ((texto[i] > -1 || texto[i] < 256 || isalpha(texto[i]) || isdigit(texto[i]) || texto[i] == '|' || texto[i] == '-' || texto[i] == ' ') && texto[i] != 39)
-		{
-			text += texto[i];
-		}
 	}
 
-	std::vector<std::string> datos = split_iterator(text);
-
-	return "(default, '" + datos[0] + "', '" + datos[1] + "', '" + datos[2] + "', '" + datos[3] + "', '" + datos[4] + "', '" + datos[5] + "', '" + datos[6] + "', '" + datos[7] + "', '" + datos[8] + "', '" + datos[9] + "', '" + datos[10] + "', '" + datos[11] + "', '" + datos[12] + "', '" + datos[13] + "', '" + datos[14] + "' ),";
-}
-
-std::string formatUpdate(std::string& texto)
-{
-	std::vector<std::string> datos = split_iterator(texto);
-	return "UPDATE empresas SET razonsocial='" + datos[1] + "',estadocontribuyente='" + datos[2] + "',condicionDomicilio='" + datos[3] + "',ubigeo='" + datos[4] + "',tipovia='" + datos[5] + "',nombrevia='" + datos[6] + "',codigozona='" + datos[7] + "',tipozona='" + datos[8] + "',numero='" + datos[9] + "',interior='" + datos[10] + "',lote='" + datos[11] + "',departamento='" + datos[12] + "',manzana='" + datos[13] + "',kilometro='" + datos[14] + "' WHERE ruc='" + datos[0] + "';\n";
-}
-
-std::string formatDelete(std::string& ruc)
-{
-	return "ruc='" + ruc + "' or ";
-}
-
-int main(int argc, const char* argv[])
-{
-
-	//setlocale(LC_ALL, "spanish"); 
-	setlocale(LC_ALL, "es_ES");
-
-
-
-	//const char* localFilePath = "padron_reducido_ruc.zip";
-	const char* localFileTxt = "ruc.txt";
-	//const char* localFileTxtTmp = "ruc_temp.txt";
-	//const char* urlPath = "http://www2.sunat.gob.pe/padron_reducido_ruc.zip";
-
-	const char* url = (argc > 1 ? argv[1] : ZAR_URL);
-	const std::string user(argc >= 3 ? argv[2] : ZAR_USER);
-	const std::string pass(argc >= 4 ? argv[3] : ZAR_PASS);
-	const std::string database(argc >= 5 ? argv[4] : ZAR_DB);
-
-	std::ofstream registrosTXT;
-	std::ofstream modificadosTXT;
-
-	registrosTXT.open("registrados.txt");
-	registrosTXT << "RUC|NOMBRE O RAZON SOCIAL|ESTADO DEL CONTRIBUYENTE|CONDICION DE DOMICILIO|UBIGEO|TIPO DE VIA|NOMBRE DE VIA|CODIGO DE ZONA|TIPO DE ZONA|NUMERO|INTERIOR|LOTE|DEPARTAMENTO|MANZANA|KILOMETRO|\n";
-
-	modificadosTXT.open("modificados.txt");
-	modificadosTXT << "RUC|NOMBRE O RAZON SOCIAL|ESTADO DEL CONTRIBUYENTE|CONDICION DE DOMICILIO|UBIGEO|TIPO DE VIA|NOMBRE DE VIA|CODIGO DE ZONA|TIPO DE ZONA|NUMERO|INTERIOR|LOTE|DEPARTAMENTO|MANZANA|KILOMETRO|\n";
-
-	//if (obtenerZip(localFilePath, urlPath))
-	//{
-	//	std::cout << "Zip instalado\n";
-	//}
-
-	//if (descomprimirZip(localFilePath))
-	//{
-	//	std::cout << "Zip descomprimido\n";
-	//}
-
-	std::ifstream archivo;
-	std::string texto;
-
-	try
-	{
-
-		sql::mysql::MySQL_Driver* driver;
-		sql::Connection* con;
-		sql::ConnectOptionsMap connection_properties;
-
-		connection_properties[OPT_HOSTNAME] = url;
-		connection_properties[OPT_USERNAME] = user;
-		connection_properties[OPT_PASSWORD] = pass;
-		connection_properties[OPT_PORT] = 3306;
-		connection_properties[OPT_SCHEMA] = database;
-		connection_properties["OPT_SSL_MODE"] = sql::SSL_MODE_DISABLED;
-		connection_properties[OPT_RECONNECT] = true;
-
-		driver = sql::mysql::get_driver_instance();
-		driver->threadInit();
-
-
-		archivo.open(localFileTxt, std::ios::in);
-
-		while (!archivo.eof())
-		{
-			getline(archivo, texto);
-			break;
-		}
-
-		unsigned int i;
-
-		std::string queryInsertar = "INSERT INTO empresas VALUES ";
-		std::list<std::string> listaQueryInsertar;
-
-
-
-		std::string query = queryInsertar;
-		std::string n_ruc = "59|LEONÉÓ VÁSQUEZ|-|-|-|-|-|-|-|-|-|-|-|-|-|";
-
-		//while (!archivo.eof())
-		//{
-		//	getline(archivo, texto);
-		//	query += formatInsert(texto);
-		//	//break;
-		//}
-
-		query += formatInsert(n_ruc);
-
-		query.back() = ';';
-
-
-		std::cout << query;
-
-		con = driver->connect(connection_properties);
-
-		std::unique_ptr<sql::PreparedStatement> pstmtModificar(con->prepareStatement(query));
-		pstmtModificar->execute();
-		std::cout << "Insertando DB\n";
-
-
-		con->close();
-		delete con;
-
-
-
-		archivo.close();
-		driver->threadEnd();
-	}
-	catch (sql::SQLException& e)
-	{
-		std::ofstream error;
-		error.open("error.txt");
-
-		error << "# ERR: SQLException in " << __FILE__ << "\n";
-		error << "(" << "EXAMPLE_FUNCTION" << ") on line " << __LINE__ << "\n";
-
-		error << "# ERR: " << e.what() << "\n";
-		error << " (MySQL error code: " << e.getErrorCode() << "\n";
-		error << ", SQLState: " << e.getSQLState() << " )" << "\n";
-
-		return EXIT_FAILURE;
-	}
-
-	//rename(localFileTxt, localFileTxtTmp);
-
-	CURL* curl;
-	FILE* fp;
-	CURLcode res;
-	char* url_ruc = "http://www2.sunat.gob.pe/padron_reducido_ruc.zip";
-	char outfilename[FILENAME_MAX] = "ruc.zip";
-	curl = curl_easy_init();
-	if (curl) {
-		fp = fopen(outfilename, "wb");
-		curl_easy_setopt(curl, CURLOPT_URL, url_ruc);
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-		res = curl_easy_perform(curl);
-		/* always cleanup */
-		curl_easy_cleanup(curl);
-		fclose(fp);
-	}
-
-
-	libzippp::ZipArchive zf("ruc.zip");
-	zf.open(ZipArchive::ReadOnly);
-
-	vector<ZipEntry> entries = zf.getEntries();
-	vector<ZipEntry>::iterator it;
-	for (it = entries.begin(); it != entries.end(); ++it) {
-		ZipEntry entry = *it;
-		string name = entry.getName();
-		int size = entry.getSize();
-
-		//the length of binaryData will be size
-		void* binaryData = entry.readAsBinary();
-
-		//the length of textData will be size
-		string textData = entry.readAsText();
-
-		cout << textData;
-		//...
-	}
-
-	zf.close();
+	hilo_1.join();
+	//hilo_2.join();
+	spdlog::info("LOSE");
 
 	return 0;
+}
 
+void DockSpace()
+{
+	static bool opt_fullscreen = true;
+	static bool opt_padding = false;
+	static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
+
+	ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+	if (opt_fullscreen)
+	{
+		ImGuiViewport* viewport = ImGui::GetMainViewport();
+		ImGui::SetNextWindowPos(viewport->WorkPos);
+		ImGui::SetNextWindowSize(viewport->WorkSize);
+		ImGui::SetNextWindowViewport(viewport->ID);
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+		window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+		window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+	}
+	else
+	{
+		dockspace_flags &= ~ImGuiDockNodeFlags_PassthruCentralNode;
+	}
+
+	if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+		window_flags |= ImGuiWindowFlags_NoBackground;
+
+	if (!opt_padding)
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+	ImGui::Begin("DockSpace Demo", NULL, window_flags);
+	if (!opt_padding)
+		ImGui::PopStyleVar();
+
+	if (opt_fullscreen)
+		ImGui::PopStyleVar(2);
+
+	// DockSpace
+	ImGuiIO& io = ImGui::GetIO();
+	if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+	{
+		ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+		ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+	}
+	else
+	{
+		ImGuiIO& io = ImGui::GetIO();
+		ImGui::Text("ERROR: Docking is not enabled! See Demo > Configuration.");
+		ImGui::Text("Set io.ConfigFlags |= ImGuiConfigFlags_DockingEnable in your code, or ");
+		ImGui::SameLine(0.0f, 0.0f);
+		if (ImGui::SmallButton("click here"))
+			io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+	}
+
+	if (ImGui::BeginMenuBar())
+	{
+		if (ImGui::BeginMenu("Options"))
+		{
+			//ImGui::MenuItem("Fullscreen", NULL, &opt_fullscreen);
+			ImGui::MenuItem("Padding", NULL, &opt_padding);
+			//ImGui::Separator();
+			ImGui::EndMenu();
+		}
+
+		ImGui::EndMenuBar();
+	}
+	ImGui::End();
+}
+
+void download_zip()
+{
+	while (true)
+	{
+		if (is_zip)
+		{
+			spdlog::info("init download");
+			CURL* curl;
+			FILE* fp;
+			CURLcode res;
+			char* url_ruc = "http://www2.sunat.gob.pe/padron_reducido_ruc.zip";
+			char outfilename[FILENAME_MAX] = "ruc.zip";
+			curl = curl_easy_init();
+			if (curl) {
+				fp = fopen(outfilename, "wb");
+				curl_easy_setopt(curl, CURLOPT_URL, url_ruc);
+				curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+				curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+				res = curl_easy_perform(curl);
+				/* always cleanup */
+				curl_easy_cleanup(curl);
+				fclose(fp);
+				spdlog::info("{} dowload succes", url_ruc);
+			}
+			is_zip = false;
+			break;
+		}
+	}
+
+}
+
+void loop()
+{
+	spdlog::info("Loop");
+
+	if (glfwInit() == GL_FALSE) return;
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	window = glfwCreateWindow(800, 600, "Programa", NULL, NULL);
+	if (!window) return;
+
+	glfwMakeContextCurrent(window);
+
+	if (GLenum err = glewInit()) return;
+
+
+
+	const char* glsl_version = "#version 330";
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+	io.ConfigFlags |= ImGuiWindowFlags_AlwaysHorizontalScrollbar;
+
+	ImGui::StyleColorsDark();
+
+	ImGuiStyle& style = ImGui::GetStyle();
+	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+	{
+		style.WindowRounding = 0.0f;
+		style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+	}
+
+	ImGuiWindowFlags window_flags = 0;
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(glsl_version);
+
+	while (!glfwWindowShouldClose(window))
+	{
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		DockSpace();
+
+
+		ImGui::Begin("Data");
+		{
+			if (ImGui::Button("Descargar zip"))
+			{
+				is_zip = true;
+
+			}
+		}
+		ImGui::End();
+
+
+
+		ImGui::ShowDemoWindow();
+
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+		{
+			GLFWwindow* backup_current_context = glfwGetCurrentContext();
+			ImGui::UpdatePlatformWindows();
+			ImGui::RenderPlatformWindowsDefault();
+			glfwMakeContextCurrent(backup_current_context);
+		}
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
 
 }
 
 
+
+//#include <iostream>  // cout
+//#include <future>  // async
+//#include <thread>
+//
+//void f() { for (auto i{ 0 }; i < 10000; ++i) std::cout << "a\n"; }
+//void g() { for (auto i{ 0 }; i < 10000; ++i) std::cout << "b\n"; }
+//
+//int main()
+//{
+//    std::future<void> fut{};
+//    if (true)  // 0 just for testing; your condition would come here
+//    {
+//        fut = std::async(std::launch::async, f);
+//    }
+//    std::thread t_g(g);
+//    t_g.join();
+//    if (fut.valid())
+//    {
+//        fut.get();
+//        std::cout << "async";
+//    }
+//}
