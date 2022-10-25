139c9b12332746bcd78378b3aee89fb7308ad157
Yordy Leonidas <yordy.lmv.2000@gmail.com>
2022-10-25 15:01:16 +0200

    insert fixed

--- src/dao/empresa_dao.h
+++ src/dao/empresa_dao.h
@@ -85,6 +85,206 @@ namespace zar
 			execute(query);
 		}
 
+		void insert(file_data*& file)
+		{
+			unsigned c = 0;
+			unsigned n_partition = 1000;
+
+			if (zar::zip::execute(file->out_filename, file->name, file->size, file->text_data))
+			{
+				zar::empresa_dao* e_dao = new zar::empresa_dao();
+				std::string data = file->text_data;
+
+				std::string query = e_dao->get_template_insert();
+				int type = 1;
+
+				spdlog::info("read {} success", file->name);
+				spdlog::warn("iterator init");
+
+				std::string::iterator it_begin = data.begin() + 188;
+				const std::string::const_iterator it_end = data.end();
+
+				zar::ruc_data new_ruc;
+				std::string r_data = "";
+
+				bool is_one_quote = false;
+				bool is_two_quote = false;
+
+				for (std::string::iterator it = it_begin; it != it_end; ++it)
+				{
+					if (*it == '|' && *(it + 1) != '|')
+					{
+						r_data = std::string(it_begin, it);
+						if (is_one_quote) {
+							zar::algorithms::fixed_one_quote(r_data);
+							is_one_quote = false;
+						}
+						else if (is_two_quote) {
+							zar::algorithms::fixed_two_quote(r_data);
+							is_two_quote = false;
+						}
+
+						zar::algorithms::set_type(new_ruc, r_data, type);
+						it_begin = it + 1;
+					}
+					else if (*it == 39)
+					{
+						is_one_quote = true;
+					}
+					else if (*it == 34)
+					{
+						is_two_quote = true;
+					}
+					else if (*it == 92)
+					{
+						*it = '/';
+					}
+					else if (*it == '\n')
+					{
+						query += e_dao->get_query_insert(new_ruc) + ",";
+						c++;
+
+						if (c >= n_partition)
+						{
+							c = 0;
+							query.back() = ';';
+
+							sql->open();
+							sql->execute_query(query);
+							sql->close();
+
+							query.clear();
+							query = e_dao->get_template_insert();
+						}
+
+						it_begin = it + 1;
+						type = 1;
+					}
+				}
+
+				rename(file->out_filename, file->out_filename_last);
+
+				query += e_dao->get_query_insert(new_ruc) + ",";
+				query.back() = ';';
+
+				sql->open();
+				sql->execute_query(query);
+				sql->close();
+
+				query.clear();
+
+				spdlog::info("finish");
+			}
+		}
+
+		void update(file_data*& file)
+		{
+			unsigned c_i = 0;
+			unsigned c_u = 0;
+			unsigned n_partition = 1000;
+
+			std::string query_insert = get_template_insert();
+			std::string query_delete = get_template_delete();
+
+			zar::zar_map ruc_map;
+			zar::zar_map ruc_map_last;
+
+			if (zip::execute(file->out_filename_last, file->name, file->size, file->text_data))
+			{
+				spdlog::info("file size last: {}", file->size);
+				algorithms::split_iterator(file->text_data, ruc_map_last);
+				file->text_data.clear();
+
+				if (zip::execute(file->out_filename, file->name, file->size, file->text_data))
+				{
+					spdlog::info("file size new: {}", file->size);
+					algorithms::split_iterator(file->text_data, ruc_map);
+
+					std::ofstream insert_file, update_file;
+					insert_file.open("inserts.txt");
+					update_file.open("updates.txt");
+
+					for (zar_map::iterator it = ruc_map.begin(); it != ruc_map.end(); ++it)
+					{
+						zar_map::iterator c_it = ruc_map_last.find(it->first);
+						if (c_it != ruc_map_last.end())
+						{
+							if (c_it->second != it->second)
+							{
+								update_file << it->second.ruc << "\n";
+
+								query_delete += get_query_delete(c_it->first) + " or ";
+								query_insert += get_query_insert(it->second) + ",";
+
+								if (c_i >= n_partition)
+								{
+									c_i = 0;
+
+									query_insert.back() = ';';
+									execute(query_insert);
+
+									query_insert = get_template_insert();
+								}
+
+								if (c_u >= n_partition)
+								{
+									c_u = 0;
+
+									query_delete.pop_back();
+									query_delete.pop_back();
+									query_delete.pop_back();
+
+									query_delete.back() = ';';
+									execute(query_delete);
+
+									query_delete = get_template_delete();
+								}
+
+								//spdlog::warn("old company");
+								//c_it->second.print();
+								//spdlog::warn("updated company");
+								//it->second.print();
+							}
+						}
+						else
+						{
+							insert_file << it->second.ruc << "\n";
+							query_insert += get_query_insert(it->second) + ",";
+
+							if (c_i >= n_partition)
+							{
+								c_i = 0;
+
+								query_insert.back() = ';';
+								execute(query_insert);
+
+								query_insert = get_template_insert();
+							}
+							/*spdlog::warn("nueva empresa");
+							it->second.print();*/
+						}
+					}
+
+					insert_file.close();
+					update_file.close();
+				}
+			}
+
+			remove(file->out_filename_last);
+			rename(file->out_filename, file->out_filename_last);
+
+			query_delete.pop_back();
+			query_delete.pop_back();
+			query_delete.pop_back();
+
+			query_delete.back() = ';';
+			query_insert.back() = ';';
+
+			execute(query_delete);
+			execute(query_insert);
+
+		}
+
 		ruc_data query(const std::string& ruc)
 		{
 			std::string query = "SELECT * FROM empresas WHERE ruc=\"" + ruc + "\"";
--- src/main.cpp
+++ src/main.cpp
@@ -10,108 +10,16 @@ int main()
 	zar::flag_data* flag = new zar::flag_data();
 	// --------------------------------------------
 
-	spdlog::info("welcome");
-	spdlog::warn("download file");
-
+	spdlog::info("insert init");
 	if (zar::http::execute(file->url, file->out_filename))
 	{
-		spdlog::info("download {} success", file->url);
-		spdlog::warn("read zip");
+		zar::mysql::instance()->connect(*server);
+		zar::empresa_dao* e_dao = zar::empresa_dao::instance();
 
-		if (zar::zip::execute(file->out_filename, file->name, file->size, file->text_data))
-		{
-			zar::mysql* sql = zar::mysql::instance();
-			sql->connect(*server);
-
-			zar::empresa_dao* e_dao = new zar::empresa_dao();
-			std::string data = file->text_data;
-
-			std::string query = e_dao->get_template_insert();
-			e_dao->create_table();
-
-			int type = 1;
-			unsigned c = 0;
-
-			unsigned line = 0;
-			unsigned n_partition = 1000;
-
-			spdlog::info("read {} success", file->name);
-			spdlog::warn("iterator init");
-
-			std::string::iterator it_begin = data.begin() + 188;
-			const std::string::const_iterator it_end = data.end();
-
-			zar::ruc_data new_ruc;
-			std::string r_data = "";
-
-			bool is_one_quote = false;
-			bool is_two_quote = false;
-
-			for (std::string::iterator it = it_begin; it != it_end; ++it)
-			{
-				if (*it == '|' && *(it + 1) != '|')
-				{
-					r_data = std::string(it_begin, it);
-					if (is_one_quote) {
-						zar::algorithms::fixed_one_quote(r_data);
-						is_one_quote = false;
-					}
-					else if (is_two_quote) {
-						zar::algorithms::fixed_two_quote(r_data);
-						is_two_quote = false;
-					}
-
-					zar::algorithms::set_type(new_ruc, r_data, type);
-					it_begin = it + 1;
-				}
-				else if (*it == 92)
-				{
-					*it = '/';
-				}
-				else if (*it == 39)
-				{
-					is_one_quote = true;
-				}
-				else if (*it == 34)
-				{
-					is_two_quote = true;
-				}
-				else if (*it == '\n')
-				{
-					query += e_dao->get_query_insert(new_ruc) + ",";
-					c++;
-
-					if (c >= n_partition)
-					{
-						c = 0;
-						query.back() = ';';
-
-						sql->open();
-						sql->execute_query(query);
-						sql->close();
-
-						query.clear();
-						query = e_dao->get_template_insert();
-					}
-
-					it_begin = it + 1;
-					type = 1;
-				}
-			}
-
-			query += e_dao->get_query_insert(new_ruc) + ",";
-			query.back() = ';';
-
-			sql->open();
-			sql->execute_query(query);
-			sql->close();
-
-			query.clear();
-
-			spdlog::info("finish");
-			rename(file->out_filename, file->out_filename_last);
-		}
-
+		e_dao->create_table();
+		e_dao->insert(file);
 	}
+	spdlog::info("insert success");
+	
 	return 0;
 }
